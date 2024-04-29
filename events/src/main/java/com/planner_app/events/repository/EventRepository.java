92895b108b18b648b16e84f0c08068a886f32e41
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-29 09:44:29 -0400

    feat: JPA repository for events

--- events/src/main/java/com/planner_app/events/repository/EventRepository.java
+++ events/src/main/java/com/planner_app/events/repository/EventRepository.java
@@ -1,7 +1,5 @@
 package com.planner_app.events.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.planner_app.events.model.EventEntity;
 
 
 @Repository
-public interface EventRepository extends JpaRepository<EventEntity, Integer> {
+public interface EventRepository extends JpaRepository<EventEntity, String> {
 }
