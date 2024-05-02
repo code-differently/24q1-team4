820d9f8debc2e3cd02eee78129267074caa03438
Anthony D. Mays <anthony@morganlatimer.com>
2024-05-02 15:34:18 -0700

    chore: employs modals component
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- EventsFront-End/src/app/page.js
+++ EventsFront-End/src/app/page.js
@@ -79,46 +79,7 @@ export default function Page() {
     <>
       <Header />
 
-      <div id="modal-signup" className="modal">
-          <div className="modal-content">
-              <h4>Sign up</h4><br />
-              <form id="signup-form">
-                  <div className="input-field">
-                      <input type="email" id="signup-email" required />
-                      <label htmlFor="signup-email">Email address</label>
-                  </div>
-                  <div className="input-field">
-                      <input type="password" id="signup-password" required />
-                      <label htmlFor="signup-password">Choose password</label>
-                  </div>
-                  <button className="btn yellow darken-2 z-depth-0">Sign up</button>
-              </form>
-          </div>
-      </div>
-
-      <div id="modal-login" className="modal">
-          <div className="modal-content">
-              <h4>Login</h4><br />
-              <form id="login-form">
-                  <div className="input-field">
-                      <input type="email" id="login-email" required />
-                      <label htmlFor="login-email">Email address</label>
-                  </div>
-                  <div className="input-field">
-                      <input type="password" id="login-password" required />
-                      <label htmlFor="login-password">Your password</label>
-                  </div>
-                  <button className="btn yellow darken-2 z-depth-0">Login</button>
-              </form>
-          </div>
-      </div>
-
-      <div id="modal-account" className="modal">
-          <div className="modal-content center-align">
-              <h4>Account details</h4><br />
-              <div className="account-details"></div>
-          </div>
-      </div>
+      <Modals />
 
       <div className="container">
           <div className="sidebar">
