91ee52e055f066654c3b000b5d53ca0c4848a24d
Aaron2278 <143147068+Aaron2278@users.noreply.github.com>
2024-05-03 09:46:55 -0400

    feat: adds feature to create event and search for existing event (#23)

--- EventsFront-End/src/app/components/MainContent.js
+++ EventsFront-End/src/app/components/MainContent.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import styles from './MainContent.module.css';
 
+function formatDate(dateInt) {
+    const str = dateInt.toString();
+    const year = str.substring(0, 4);
+    const month = str.substring(4, 6);
+    const day = str.substring(6, 8);
+    return `${year}-${month}-${day}`;
+}
+
 export default function MainContent({ event }) {
-  return (
-    <div className={styles['main-content']}>
-        <div className={styles['outer-box']}>
-            <div className={styles['background-image']}></div>
-            <div className={styles.card}>
-                <div className={styles['card-inner']}>
-                    <div className={styles['card-front']}>You're Invited</div>
-                    <div className={styles['card-back']}>
-                        <h2>{event?.eventName}</h2>
-                        <p>Event Details</p>
-                        <button className={styles['trash-button']}><i className={styles['gg-trash']}></i></button>
-                        <button className={styles['cloud-button']}><i className={styles['gg-cloud']}></i></button>
+    return (
+        <div className={styles['main-content']}>
+            <div className={styles['outer-box']}>
+                <div className={styles.card}>
+                    <div className={styles['card-inner']}>
+                        <div className={styles['card-front']}>
+                            You're Invited
+                        </div>
+                        <div className={styles['card-back']}>
+                            <h2>{event?.eventName || 'Event Name'}</h2>
+                            <p>Date: {event ? formatDate(event.eventDate) : 'Event Date'}</p>
+                            <p>Head Count: {event?.headCount || 'Head Count'}</p>
+                            <button className={styles['trash-button']}><i className={styles['gg-trash']}></i></button>
+                            <button className={styles['cloud-button']}><i className={styles['gg-cloud']}></i></button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
